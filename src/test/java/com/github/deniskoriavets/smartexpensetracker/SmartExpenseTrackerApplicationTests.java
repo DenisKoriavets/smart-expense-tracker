5f2684dfb80508deae9e3988ba1c62ccf5596d36
DenisKoriavets <d.korjavets@ukr.net>
2026-03-01 18:29:11 +0200

    Init project structure

---
+++ src/test/java/com/github/deniskoriavets/smartexpensetracker/SmartExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.deniskoriavets.smartexpensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartExpenseTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
