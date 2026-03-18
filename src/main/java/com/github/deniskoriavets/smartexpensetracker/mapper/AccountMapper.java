98ead5a67da0eb18e9dde3900db26fca8a4c1b25
DenisKoriavets <d.korjavets@ukr.net>
2026-03-18 10:34:20 +0200

    feat: add DTOs and MapStruct mapper for Account

---
+++ src/main/java/com/github/deniskoriavets/smartexpensetracker/mapper/AccountMapper.java
@@ -0,0 +1,19 @@
+package com.github.deniskoriavets.smartexpensetracker.mapper;
+
+import com.github.deniskoriavets.smartexpensetracker.dto.account.AccountResponseDto;
+import com.github.deniskoriavets.smartexpensetracker.dto.account.CreateAccountDto;
+import com.github.deniskoriavets.smartexpensetracker.dto.account.UpdateAccountDto;
+import com.github.deniskoriavets.smartexpensetracker.entity.Account;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface AccountMapper {
+    Account toEntity(CreateAccountDto createAccountDto);
+
+    @Mapping(target = "balance", ignore = true)
+    AccountResponseDto toResponseDto(Account account);
+
+    void updateAccount(@MappingTarget Account account, UpdateAccountDto updateAccountDto);
+}
